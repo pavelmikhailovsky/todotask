78ea21b1c03cfae17b6306536054be8daff3cacb
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-24 16:22:30 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+***
+### Project description
+***This is the backend part of the todotask project.***
+***
---
+++ src/main/java/org/todotask/config/WebMvcApplicationConfig.java
@@ -0,0 +1,39 @@
+package org.todotask.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ComponentScan("org.todotask")
+@PropertySource("classpath:application.properties")
+@EnableWebMvc
+public class WebMvcApplicationConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource db = new DriverManagerDataSource();
+        db.setDriverClassName(Objects.requireNonNull(env.getProperty("database.driver")));
+        db.setUrl(Objects.requireNonNull(env.getProperty("database.url")));
+        db.setUsername(Objects.requireNonNull(env.getProperty("database.username")));
+        db.setPassword(Objects.requireNonNull(env.getProperty("database.password")));
+        return db;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
