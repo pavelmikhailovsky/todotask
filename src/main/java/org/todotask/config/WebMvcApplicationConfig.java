cdae1091abec3c77f91d828f32a5e5d4c6ed3467
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-11-24 23:30:01 +0300

    added swagger and openapi but it is not work

--- src/main/java/org/todotask/config/WebMvcApplicationConfig.java
+++ src/main/java/org/todotask/config/WebMvcApplicationConfig.java
@@ -1,6 +1,13 @@
 package org.todotask.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.sql.DataSource;
+import java.util.List;
 import java.util.Objects;
 
 @Configuration
@@ -43,5 +51,29 @@ public class WebMvcApplicationConfig implements WebMvcConfigurer {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public GroupedOpenApi publicUserApi() {
+        return GroupedOpenApi.builder()
+                .group("Users")
+                .pathsToMatch("/user/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi(@Value("${openapi.application-description}")String appDescription,
+                                 @Value("${openapi.application-version}")String appVersion) {
+        return new OpenAPI().info(new Info().title("TODOTASK API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .license(new License().name("Apache 2.0")
+                                .url("http://springdoc.org"))
+                        .contact(new Contact().name("username")
+                                .email("test@gmail.com")))
+                .servers(List.of(new Server().url("http://localhost:8080")
+                                .description("Dev service"),
+                        new Server().url("http://localhost:8082")
+                                .description("Beta service")));
     }
 }
--- src/main/java/org/todotask/controller/UserController.java
+++ src/main/java/org/todotask/controller/UserController.java
@@ -1,5 +1,12 @@
 package org.todotask.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +15,7 @@ import org.todotask.service.UserService;
 
 import java.util.List;
 
+@Tag(name = "User", description = "The User API")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -19,6 +27,19 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Gets all users", tags = "user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Founds all users",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = User.class))
+                            )
+                    }
+            )
+    })
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public List<User> getAllUsers() {
